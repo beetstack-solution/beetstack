4d0bfe19359c20b2bb4868aa7efc421ce0d18bfa
ajmal faris <ajmalfaris.me@gmail.com>
2026-03-28 16:47:09 +0530

    feat(web): initial web application setup

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@repo/ui/tailwind.config";
+
+const config: Pick<Config, "content" | "presets"> = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  presets: [sharedConfig],
+};
+
+export default config;
