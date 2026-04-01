3d323582d4b77707ad004f378cb6d0b0fbc99884
ajmal faris <ajmalfaris.me@gmail.com>
2026-04-01 09:55:06 +0530

    refactor: update tailwind content paths and finalize features directory removal

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,6 @@ import sharedConfig from "@repo/ui/tailwind.config";
 const config: Pick<Config, "content" | "presets" | "theme"> = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/icons/src/**/*.{js,ts,jsx,tsx,mdx}"
